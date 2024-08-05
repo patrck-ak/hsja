aca8141f0aff1fdcd3d0b32b42f0c37dd63b02e7
patrck-ak <patrickoliveira.silva20@gmail.com>
2024-08-05 13:03:29 -0300

    atualização final

--- listagem.php
+++ listagem.php
@@ -15,7 +15,7 @@
       integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+      </script>
     <!--   -->
 
   </head>
@@ -332,7 +332,7 @@
     <!-- icones -->
     <script src="https://unpkg.com/lucide@latest"></script>
     <script>
-    lucide.createIcons();
+      lucide.createIcons();
     </script>
   </body>
 
